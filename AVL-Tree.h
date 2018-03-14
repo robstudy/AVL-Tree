c35ae7cb946283e1f720bf68c10ea47a4935c92c
Robert Garza <robstudent1988@gmail.com>
2018-03-13 17:12:49 -0700

    implemented insert function

--- AVL-Tree.h
+++ AVL-Tree.h
@@ -6,23 +6,44 @@ template <class T>
 class AVL_Tree 
 {
 	public:
-	
-	
-	
-	
+		AVL_Tree();
+		void insert(T);
 	
 	private:
-	struct Node {
-		Node *parent;
-		Node *left;
-		Node *right;
-		T data;
-		Node(Node *p = NULL, Node *l = NULL, Node *r = NULL, T d = NULL):
-			parent(p),
-			left(l),
-			right(r),
-			data(d){}
-	};
+		struct Node {
+			Node *parent;
+			Node *left;
+			Node *right;
+			T data;
+			Node(Node *p = NULL, Node *l = NULL, Node *r = NULL, T d = NULL):
+				parent(p),
+				left(l),
+				right(r),
+				data(d){}
+		};
 	
-	Node *head;
+		Node *head;
+		int size;
+};
+
+template <class T>
+AVL_Tree<T>::AVL_Tree() {
+	head = NULL;
+	size = 0;
+};
+
+template <class T>
+void AVL_Tree<T>::insert(T data) {
+	if (!head) {
+		head = new Node(NULL, NULL, NULL, data);
+		return;
+	}
+	Node tmp = head;
+	while(tmp->left != NULL && tmp->right != NULL) {
+		if(tmp->data < data && tmp->right != NULL) tmp = tmp->right;
+		else tmp = tmp->left;
+	}
+	
+	if(data >= tmp->data) tmp->right = new Node(tmp, NULL, NULL, data);
+	else tmp->left = new Node(tmp, NULL, NULL, data);
 };
